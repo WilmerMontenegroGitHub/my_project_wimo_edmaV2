d2c8642d166e130e26ff3831e925d88cbe785d52
edisonmanjarre <edison.manjarres@gmail.com>
2018-06-08 22:29:40 -0500

    prueba de clonar el proyect

--- src/odd_number.py
+++ src/odd_number.py
@@ -1,8 +1,5 @@
 class OddNumber:
 
     @staticmethod
-    #print ('ejecutando OddNumber...')
-    def odd_number(number):
-        if number % 2 == 0:
             return True
         return False
