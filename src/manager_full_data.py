a590cbb9652b35cb5653691e22649cfaf5b29780
edisonmanjarre <edison.manjarres@gmail.com>
2018-06-22 22:41:59 -0500

    verifico datos principales de la muestra con describe e info i Please enter the commit message for your changes. Lines starting

--- src/manager_full_data.py
+++ src/manager_full_data.py
@@ -1,13 +1,27 @@
-from pandas import pandas
+from pandas \
+import pandas as pd
 import os
 
 def main():
-    print ('hola')
+    print ('carga de archivo')
+    print ('................')
+    print('\n')
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, '../data/FormatoDeAlmacenamiento1.csv')
-    df = pandas.read_csv(filename, sep=';', header=None, na_values=" NaN")
+    df = pd.read_csv(filename, sep=';', header=None, na_values=" NaN")
 
-    print (df)
-
+    #print (df)
+    #imprimo la informacion del archivo que cargo
+    print (df.info())
+    print ('\n'*2)
+    print(df.describe())
+    #limpio datos
+    #
+    print ('\n'*2)
+    #nuevo=pd.DataFrame(df)
+    #nuevo=nuevo.replace("","NO")
+    #print (nuevo)
+    columna =df[df[1]!='NaN']
+    #print(columna)
 if __name__ == '__main__':
     main()
